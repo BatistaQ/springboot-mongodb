cd9e788d521b5e3407ce6023770b765f6fa61faa
BatistaQ <kauanbs29@hotmail.com>
2024-12-20 11:13:03 -0300

    connecting to mongodb with repository and service

---
+++ src/main/java/com/project/mongodb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.mongodb.repository;
+
+import com.project.mongodb.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/project/mongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.project.mongodb.services;
+
+import com.project.mongodb.domain.User;
+import com.project.mongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
